1ec892b23301643db1029dd40168694d9bb6602c
Roman <romanslex@gmail.com>
2019-02-26 10:17:06 +0300

    Добавил настройки webpack

---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+    entry: {},
+    output: {
+        filename: "js/[name].js",
+        path: path.resolve(__dirname, "public")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader"
+            },
+            {
+                test: /\.styl(us)?$/,
+                use: [
+                    'vue-style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-plain-loader'
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: "[sha512:hash:base64:7].[ext]",
+                        outputPath: "/img/",
+                    }
+                }]
+            },
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css'
+        })
+    ]
+};
