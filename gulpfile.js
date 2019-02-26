9dfc1842b13cb16d84494004fdb08c795ee2b0da
Roman <romanslex@gmail.com>
2019-02-26 10:53:55 +0300

    Файл theme.scss содержит bootstrap тему

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 const gulp = require("gulp");
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
 const webpackStream = require("webpack-stream");
 
 gulp.task("webpack-dev", () =>
@@ -10,4 +13,10 @@ gulp.task("webpack-prod", function () {
     return gulp.src(".")
         .pipe(webpackStream(require("./webpack.prod.js")))
         .pipe(gulp.dest("public"))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./resources/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
 });
