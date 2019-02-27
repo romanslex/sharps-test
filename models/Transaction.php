bfe780924eace87ac9fcdbadfd59d99c0399d7fe
Roman <romanslex@gmail.com>
2019-02-27 10:24:52 +0300

    Добавил в модель транзакции методы для получения плательщика и получателя

--- models/Transaction.php
+++ models/Transaction.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $guarded = [];
+   
+    public $timestamps = false;
+
+    protected $dates = ['performed_at'];
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'id', 'payer_id');
+    }
+
+    public function recipient()
+    {
+        return $this->belongsTo(User::class, 'id', 'recipient_id');
+    }
 }
