48bbee697aca8f1438741d89c6a9cc2730eaf6c0
Roman <romanslex@gmail.com>
2019-02-27 10:38:29 +0300

    Сделал seeder для работы

---
+++ database/seeds/DevSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Models\Transaction;
+use Models\User;
+
+class DevSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createMainUser();
+        $users = factory(User::class, 9)->create();
+
+        foreach($users as $payer)
+        {
+            foreach($users as $recipient){
+                if($payer->id === $recipient->id)
+                    continue;
+
+                factory(Transaction::class)->create([
+                    'payer_id' => $payer->id,
+                    'recipient_id' => $recipient->id
+                ]);
+            }
+        }
+    }
+
+    public function createMainUser(){
+        factory(User::class)->create([
+            'name' => 'Roman',
+            'email' => 'example@mail.ru',
+            'password' => Hash::make('secret')
+        ]);
+    }
+}
