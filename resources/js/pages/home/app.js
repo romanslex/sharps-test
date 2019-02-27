7a4d97c2f8bda6660630690ff0b4ef3bf9d7adff
Roman <romanslex@gmail.com>
2019-02-27 20:07:15 +0300

    Установил vuex Добавил store

--- resources/js/pages/home/app.js
+++ resources/js/pages/home/app.js
@@ -3,12 +3,14 @@ import Vue from 'vue'
 import App from './components/App.vue'
 import Vuelidate from 'vuelidate'
 Vue.use(Vuelidate);
+import store from './store';
 
 let transactions = JSON.parse(document.getElementById('app').dataset['transactions']);
 let user = JSON.parse(document.getElementById('app').dataset['user']);
 let users = JSON.parse(document.getElementById('app').dataset['users']);
 
 const app = new Vue({
+    store,
     render: h => h(App, {
         props: {
             transactions,
