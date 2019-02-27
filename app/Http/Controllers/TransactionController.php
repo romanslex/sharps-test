80119cebf866fb5e97ea2e2e810e86855e851737
Roman <romanslex@gmail.com>
2019-02-27 16:30:29 +0300

    Перевод денег работает

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Models\User;
+use mysql_xdevapi\Exception;
 
 class TransactionController extends Controller
 {
     public function create(Request $request)
     {
-        return [];
+        $user = auth()->user()->firstOrFail();
+
+        $request->validate([
+            'recipient' => 'required|exists:users,id',
+            'amount' => 'required|numeric|lte:' . $user->balance
+        ]);
+
+        $recipient = User::findOrFail($request->get('recipient'));
+        $amount = $request->get('amount');
+
+        \DB::transaction(function() use ($user, $recipient, $amount) {
+            $user->balance -= $amount;
+            $recipient->balance += $amount;
+            $user->save();
+            $user->outboundTransactions()->create([
+                'recipient_id' => $recipient->id,
+                'amount' => $amount,
+                'balance' => $user->balance
+            ]);
+            $recipient->save();
+        });
     }
 }
