392a514543dab75c78c42b6ccefe214829341ce9
Roman <romanslex@gmail.com>
2019-02-27 13:20:15 +0300

    Создал HomeViewModel и переместил логику формирования массива транзакций туда

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\ViewModels\HomeViewModel;
 use Illuminate\Http\Request;
 use Models\User;
 
@@ -29,24 +30,11 @@ class HomeController extends Controller
             ->with(['outboundTransactions.recipient', 'inboundTransactions'])
             ->firstOrFail();
 
-        $outboundTransactions = $user->outboundTransactions->map(function ($item) {
-            return [
-                'performed_at' => $item->performed_at->format('Y-m-d H:i'),
-                'name' => $item->recipient->name,
-                'amount' => $item->amount,
-                'balance' => 0
-            ];
-        });
-
-        $inboundTransactions = $user->inboundTransactions->map(function ($item) use ($user) {
-            return [
-                'performed_at' => $item->performed_at->format('Y-m-d H:i'),
-                'name' => $user->name,
-                'amount' => $item->amount,
-                'balance' => 0
-            ];
-        });
-        $transactions = $inboundTransactions->merge($outboundTransactions);
-        return view('home', ['transactions' => $transactions]);
+        $viewModel = new HomeViewModel(
+            $user,
+            $user->outboundTransactions,
+            $user->inboundTransactions
+        );
+        return view('home', ['vm' => $viewModel]);
     }
 }
