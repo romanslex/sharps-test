6f0cbf49ca58ae023864d9af3c2c51a8e8be4a01
Roman <romanslex@gmail.com>
2019-02-28 13:12:44 +0300

    Добавил vuex store к admin приложению

--- resources/js/pages/admin/app.js
+++ resources/js/pages/admin/app.js
@@ -2,8 +2,10 @@ require('../../bootstrap');
 import Vue from 'vue'
 import App from './components/App.vue'
 import router from './router'
+import store from './store'
 
 const app = new Vue({
+    store,
     router,
     render: h => h(App),
 }).$mount('#app');
