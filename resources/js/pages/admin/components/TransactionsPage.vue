afb594d70a7494179166c13338a7de0b2efae27c
Roman <romanslex@gmail.com>
2019-02-28 15:43:49 +0300

    Таблица для транзакций

--- resources/js/pages/admin/components/TransactionsPage.vue
+++ resources/js/pages/admin/components/TransactionsPage.vue
@@ -1,13 +1,66 @@
 <template lang="pug">
     .card
         .card-header Transactions page
-        .card-body Hi
+        .card-body
+            table.table
+                tr
+                    th(v-for="column in columns" :key="column.key")
+                        input.form-control(
+                        type="search"
+                        v-if="column.type === 'string'"
+                        v-model="column.filterValue"
+                        @input="onFilterChange()"
+                        placeholder="Filter"
+                        )
+                        date-picker(
+                        input-class='form-control'
+                        v-if="column.type === 'datetime'"
+                        v-model="column.filterValue"
+                        lang="en"
+                        range
+                        @change="onFilterChange()"
+                        )
+                tr
+                    th.header(v-for="column in columns" :key="column.name" @click="onSortClick(column)")
+                        | {{column.name}}
+                        i.fas.fa-sort-amount-up(v-show="column.sort && !column.sortDesc")
+                        i.fas.fa-sort-amount-down(v-show="column.sort && column.sortDesc")
+                tr(v-for="transaction in data" :key="transaction.id")
+    
+
 </template>
 
 <script>
-   export default {}
+    import {Column} from "../../shared/Column";
+    import DatePicker from 'vue2-datepicker';
+
+    export default {
+        components: {
+            DatePicker,
+        },
+        data() {
+            return {
+                columns: [
+                    new Column('Payer Name', 'payer_name', Column.stringFilter),
+                    new Column('Recipient Name', 'recipient_name', Column.stringFilter),
+                    new Column('Amount, PW', 'amount', Column.stringFilter),
+                    new Column('Performed At', 'performed_at', Column.datetimeFilter)
+                ]
+            }
+        },
+        computed: {
+            data() {
+                return this.$store.state.transactions;
+            }
+        },
+    }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+    .table
+        >>> svg
+            display none
 
+        >>> .mx-datepicker-range
+            width 210px
 </style>
